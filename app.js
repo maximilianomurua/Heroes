68fc38132e6deda23fffcddc2fc06daa0a66c6d0
maximilianomurua <m.javiermurua@gmail.com>
2021-06-06 17:33:00 -0300

    clarke finalizado

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static("DH-Heroes/public"));
-app.use('/recursos' , express.static(__dirname + "/DH-Heroes/public"))
-
+app.use("/recursos", express.static(__dirname + "/DH-Heroes/public"));
 
 app.listen(3030, () => console.log("Servidor funcionando"));
 
@@ -18,4 +17,24 @@ app.get("/babbage", (req, res) => {
 
 app.get("/berners-lee", (req, res) => {
   res.sendFile(path.join(__dirname, "./DH-Heroes/views/berners-lee.html"));
+});
+
+app.get("/clarke", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/clarke.html"));
+});
+
+app.get("/hamilton", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/hamilton.html"));
+});
+
+app.get("/hopper", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/hopper.html"));
+});
+
+app.get("/lovelace ", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/lovelace.html"));
+});
+
+app.get("/turing", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/turing.html"));
 });
