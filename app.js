cb36c8709a8b8173a2a847cd61cc7ea2d2a70502
maximilianomurua <m.javiermurua@gmail.com>
2021-06-06 04:32:12 -0300

    index terminado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("DH-Heroes/public"));
+app.use('/recursos' , express.static(__dirname + "/DH-Heroes/public"))
+
+
+app.listen(3030, () => console.log("Servidor funcionando"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./DH-Heroes/views/index.html"));
+});
